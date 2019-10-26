ca672f7d1647aaeb7ad2d2b11fa1539b4cb92864
Yuiki <10101661+Yuiki@users.noreply.github.com>
2019-10-26 19:44:50 +0900

    add the home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,51 @@
 import React from "react"
-import logo from "./logo.svg"
-import "./App.css"
+import logo from "./logo.png"
+import { css } from "@emotion/core"
+import { Menu } from "semantic-ui-react"
+import Slideshow from "./Slideshow"
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      <div
+        css={css({
+          backgroundColor: "#ffffffbb",
+          position: "fixed",
+          width: "100vw",
+          zIndex: 1
+        })}
+      >
+        <div
+          css={css({
+            width: "100%",
+            display: "flex"
+          })}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <img
+            src={logo}
+            css={css({ marginRight: "auto", marginLeft: "20px" })}
+            alt="logo"
+          />
+          <Menu text css={css({ marginRight: "20px !important" })}>
+            <Menu.Item name="about" />
+            <Menu.Item name="members" />
+            <Menu.Item name="activities" />
+            <Menu.Item name="exhibitions" />
+            <Menu.Item name="gallery" />
+            <Menu.Item name="contact" />
+          </Menu>
+        </div>
+      </div>
+      <Slideshow
+        imageUrls={[
+          "https://pbs.twimg.com/media/DN3xyoOUIAEb0sE?format=jpg&name=4096x4096",
+          "https://pbs.twimg.com/media/D9QmfeRU4AA2Opz?format=jpg&name=4096x4096",
+          "https://pbs.twimg.com/media/D8gjDA_UIAEk8OV?format=jpg&name=4096x4096",
+          "https://pbs.twimg.com/media/D7VV8AkUIAA6ksT?format=jpg&name=large",
+          "https://pbs.twimg.com/media/D3mc_PZV4AED70c?format=jpg&name=4096x4096"
+        ]}
+      />
+    </>
   )
 }
 
---
+++ src/Slideshow.tsx
@@ -0,0 +1,43 @@
+import React, { useEffect, useState, useRef } from "react"
+import { css } from "@emotion/core"
+
+type Props = {
+  imageUrls: string[]
+}
+
+const Slideshow: React.FC<Props> = (props) => {
+  const { imageUrls } = props
+
+  const [urlIdx, setUrlIdx] = useState(0)
+
+  const ref = useRef({ imageUrls })
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      const isLast = ref.current.imageUrls.length - 1
+      setUrlIdx(urlIdx !== isLast ? urlIdx + 1 : 0)
+    }, 5000)
+    return () => clearTimeout(timer)
+  }, [urlIdx])
+
+  return (
+    <>
+      {imageUrls.map((url, idx) => (
+        <img
+          key={url}
+          css={css({
+            width: "100vw",
+            height: "100vh",
+            objectFit: "cover",
+            position: "absolute",
+            opacity: idx === urlIdx ? "1" : "0",
+            transition: "opacity 1s ease"
+          })}
+          src={url}
+          alt="cover"
+        />
+      ))}
+    </>
+  )
+}
+
+export default Slideshow
