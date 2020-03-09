f0676ba962be993646389fc56b411108ee1601bd
Yuiki <10101661+Yuiki@users.noreply.github.com>
2020-03-09 15:15:42 +0900

    change blogs path

--- gatsby-node.js
+++ gatsby-node.js
@@ -53,7 +53,10 @@ exports.onCreateNode = ({ node, actions, getNode }) => {
   const { createNodeField } = actions
 
   if (node.internal.type === `MarkdownRemark`) {
-    const value = createFilePath({ node, getNode })
+    let value = createFilePath({ node, getNode })
+    if (node.fileAbsolutePath.includes("content/blog")) {
+      value = `/blogs${value}`
+    }
     createNodeField({
       name: `slug`,
       node,
